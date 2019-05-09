c58d290ca35508534d6caead5b804d7438e69746
Bruno Anglés <bruno.angles@pfsgroup.es>
2019-05-09 17:49:07 +0200

    Try basic auth

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const webdav = require('webdav-server').v2;
 
+// User manager (tells who are the users)
+const userManager = new webdav.SimpleUserManager();
+const user = userManager.addUser('pepe', 'pepe1234', true);
+
+// Privilege manager (tells which users can access which files/folders)
+const privilegeManager = new webdav.SimplePathPrivilegeManager();
+privilegeManager.setRights(user, '/', [ 'all' ]);
+
 const server = new webdav.WebDAVServer({
+    // HTTP Digest authentication with the realm 'Default realm'
+    httpAuthentication: new webdav.HTTPDigestAuthentication(userManager, 'Default realm'),
+    // privilegeManager: privilegeManager,
     port: process.env.PORT || 1900
 });
 
