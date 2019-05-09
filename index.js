eefe1f90a4350c4f9bc662c7283e487329877a9a
Bruno Anglés <bruno.angles@pfsgroup.es>
2019-05-09 17:01:31 +0200

    Pick server port form envirnoment variable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const webdav = require('webdav-server').v2;
 
 const server = new webdav.WebDAVServer({
-    port: 1900
+    port: process.env.PORT || 1900
 });
 
 server.afterRequest((arg, next) => {
